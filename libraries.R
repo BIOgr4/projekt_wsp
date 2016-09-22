e990c6dc048326b5a9f2949ac7a98bfa35f71fbd
sepiwo <sepiwo@gmail.com>
2016-09-22 04:05:26 +0200

    test wilcoxona

---
+++ libraries.R
@@ -0,0 +1,8 @@
+#requied libraries
+library("ALL")
+library("affyPLM")
+library("affy")
+library("hexbin")
+library("geneplotter")
+library("RColorBrewer")
+library("limma")
---
+++ selekcja.R
@@ -0,0 +1,11 @@
+#selekcja cech 
+
+ekspresje=exprs(ExampleSet)
+ExampleSet@phenoData@data$annotation
+
+roznicujace=vector(length = nrow(ekspresje))
+
+for(i in 1:nrow(ekspresje))
+{
+  =
+}

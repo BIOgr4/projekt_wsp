87e9ab2fd5f08ebd91b4e1e6f982b5e2c133417e
juuzia <emilia.manko@gmail.com>
2016-09-28 10:09:38 +0100

    jest super

--- Klasteryzacja.R
+++ Klasteryzacja.R
@@ -1,12 +1,10 @@
-
-Funkcja = function(ExampleSet,iloœæ_Kl){
+Funkcja <- function(ExampleSetExp,ilosc_Kl){
   
-  Dane=exprs(ExampleSet)
-  X=Dane[1:4,1:4]  # sa tylko 4 pliki CEL w ExpressionSet
+  X=ExampleSetExp
   X2=log(X)
   
   # Principal Component Analysis 
-  PCA=prcomp(X, cor = FALSE, scores = TRUE)  #sortowanie danych wdl. malej¹cych wariancji
+  PCA=prcomp(X, cor = FALSE, scores = TRUE)  #sortowanie danych wdl. malej?cych wariancji
   PCA2=prcomp(X2, cor = FALSE, scores = TRUE)
   
   # Skalowanie wielowymiarowe
@@ -14,19 +12,32 @@ Funkcja = function(ExampleSet,iloœæ_Kl){
   cmds = cmdscale(manDist)
   manDist2=dist(X2)
   cmds2 = cmdscale(manDist2)
-
+  
   #klasteryzacja K-Means
-  kl=kmeans(X,iloœæ_Kl)$cluster
-  kl2=kmeans(X2,iloœæ_Kl)$cluster
+  kl=kmeans(X,ilosc_Kl)$cluster
+  kl2=kmeans(X2,ilosc_Kl)$cluster
   
   #plotowanie 
-  oldpar <- par(mfrow = c(1, 2))
-  x11();par(las=1, cex=0.7, mfcol=c(2,2), oma=c(0,0,0,0))
+  par(mfrow = c(2, 2))
   plot(PCA$x[,1], PCA$x[,2], main="Dane wyjsciowe", col=kl)
   plot(PCA2$x[,1], PCA2$x[,2], main="Dane zlogarytmowane", col=kl2)
   plot(cmds[,1], cmds[,2], main="Dane wyjsciowe", col=kl)
   plot(cmds2[,1], cmds2[,2], main="Dane zlogarytmowane", col=kl2)
   title("K-Means",outer=TRUE)
-  par(oldpar)  
+  
 }
 
+Cluster_function <- function(ExampleSet, ilosc_Kl){
+  
+  data <- ExampleSet
+  cl <- labels(data)[[1]]
+  d <- dist(data)
+  par(mfrow = c(1,2))
+  image(as.matrix(d))
+  hc <- hclust(d, method = 'complete')
+  plot(hc, labels = cl)
+  groups <- cutree(hc, k = ilosc_Kl)
+  result <- as.matrix(groups, labels(cl))
+  
+  return(result)
+}

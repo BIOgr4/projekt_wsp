8a6e3db153cd53cc631bd173727e2a271277e48b
ania293 <turkiewicz293@gmail.com>
2016-09-28 09:04:04 +0100

    klasteryzacja-zmiany

--- server.R
+++ server.R
@@ -1,7 +1,8 @@
 source("F1M.R") #kontrola jakosci
+source("Klasteryzacja.R")
 
 options(shiny.maxRequestSize = 500*1024^2)
-shinyServer(function(input, output){
+shinyServer(function(input, output, session){
   
   ## input data 
   myData <- reactive({
@@ -15,6 +16,30 @@ shinyServer(function(input, output){
     eval(var.name)
   })
   
+  ## measure
+  rangeMax <- reactive({
+    if (!is.null(myData())) { 
+      Data <- myData()
+      myDataExp <- exprs(Data)
+      maxData <- dim(myDataExp)[2]
+    }
+  })
+  
+  #output$max <- renderUI({
+  #  if (!is.null(myData())) { 
+  #    rMax <- rangeMax()
+  #    browser()
+  #    sliderInput("max", "Zakres:", min=1, max=rMax, value=rMax, step=1)
+  #  }
+  #})
+  #}
+  
+  observe({
+    if (!is.null(myData())) { 
+      rMax <- rangeMax()
+      updateSliderInput(session, "range", value = c(1,rMax), min = 1, max = rMax, step = 1)
+    }
+  })
   ######################################### Raw data ############################################ 
   output$dataMessage <- renderUI ({
     if (is.null(myData())) { 
@@ -29,65 +54,85 @@ shinyServer(function(input, output){
     }
   })
   
+  
   ######################################### Results ############################################  
-    
   
-  output$results <- renderUI ({
-    if (is.null(myData())) {
-      br()
-      h3("Proszę wrzucić plik ExpressionSet")
-    }
-  })  
-  
-  output$results <- renderUI ({
+  #output$results <- renderUI ({
+  observe ({
     if (!is.null(myData())) {
       myDataExp <- exprs(myData())
-      maxData <- dimData()
+      #maxData <- dim(Data)
       
-  ################################### Quality ###################################################
+      ################################### Quality ###################################################
+      
+      if (input$menu == "quality") {
+        #browser()
+        output$plots <- renderPlot({
+          if (input$plot_type == "HEXBIN") {
+            HB(myDataExp, input$range)
+          }
+          else if (input$plot_type == "MVA"){
+            MVA(myDataExp, c(input$range[1]:input$range[2]))
+          }
+          else if (input$plot_type == "BOXPLOT") {
+            BOXY(myData(), c(input$range[1]:input$range[2]))
+          }
+          else if (input$plot_type == "SCATTER PLOT") {
+            browser()
+            SCATTER(myDataExp, c(input$range[1]:input$range[2]))
+          }
+          else if (input$plot_type == "HEATMAP") {
+            HM(myDataExp,input$range[2])
+          }
+          else if (input$plot_type == "MA PLOT") {
+            MAPLOTS(myData(),c(input$range[1]:input$range[2]))
+          }
+          
+        })
         
-        if (input$option == "quality") {
-          output$plots <- renderPlot({
-            if (input$plot_type == "HEXBIN") {
-              HB(myDataExp, c(input$range[[1]]:input$range[[2]]))
-              #img(src = name[1], align="center")
-            }
-            else if (input$plot_type == "MVA"){
-              MVA(myDataExp, c(input$range[[1]]:input$range[[2]]))
-            }
-            else if (input$plot_type == "BOXPLOT") {
-              BOXY(myDataExp, c(input$range[[1]]:input$range[[2]]))
-            }
-            else if (input$plot_type == "SCATTER PLOT") {
-              SCATTER(myDataExp, c(input$range[[1]]:input$range[[2]]))
-            }
-            else if (input$plot_type == "HEATMAP") {
-              HM(myDataExp,input$range[[2]])
-            }
-            else if (input$plot_type == "MA PLOT") {
-              MAPLOTS(mydata,c(input$range[[1]]:input$range[[2]]))
-            }
+      }
+      ################################### Classification  ############################################
+      else if (input$menu == "class") {
+        
+      }
+      
+      ################################### Clasterization  ############################################
+      else if (input$menu == "clast") {
+
+        observeEvent(input$start, {
+          output$plot_clast <- renderPlot({
+            if(input$clust_type == "K-means"){
             
+              Funkcja(myDataExp,input$num_clast)
+            } else if(input$clust_type == "Hierarchiczna"){
+            
+              Cluster_function(myDataExp, input$num_clast)
+              
+          }
+        })
+          
+            output$table_clust <- renderTable({
+              Cluster_function(myDataExp, input$num_clast)
+            }, include.rownames=TRUE)
+         })
+        observeEvent(input$stop, {
+          output$plot_clast <- renderPlot({
+            return()
+          })
+          output$table_clust <- renderTable({
+            return()
           })
           
-        }
-        ################################### Classification  ############################################
-        else if (input$option == "class") {
-          
-        }
-        
-        ################################### Clasterization  ############################################
-        else if (input$option == "clast") {
-          
-        }
+        })
       }
-    })
-    
-   
-    
+    }
+  })
   
- 
- 
+  
+  
+  
+  
+  
   
   ######################################### Download ############################################  
   
--- ui.R
+++ ui.R
@@ -1,39 +1,50 @@
 shinyUI(fluidPage(
   titlePanel("Analiza mikromacierzowa"),
   
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file_ES", label = h4("Wprowadź plik:"), multiple = FALSE, accept = c(".Rda")),
-      
-      selectInput("option", "Opcje:", c("Dane"="raw",
-                                        "Klasteryzacja"="clast",
-                                        "Klasyfikacja"="class",
-                                        "Kontrola jakości"="quality"),
-                                        selected="quality", multiple=FALSE),
+  fileInput("file_ES", label = h4("Wprowadź plik:"), multiple = FALSE, accept = c(".Rda")),
+  
+  navbarPage("Menu", id="menu",
+             tabPanel("Opis"),
+             tabPanel("Dane",value="abatch"),
+             navbarMenu("Opcje", 
+                        tabPanel("Selekcja"),
+                        "Dane przed selekcja",
+                        "Dane po selekcji",
+                        tabPanel("Klasyfikacja", value="class"),
+                        tabPanel("Klasteryzacja", value="clast"),                             
+                        tabPanel("Kontrola jakości",value="quality")),
+             tabPanel("Pobieranie wyników")),
+  
+  
+  fluidRow(
+    conditionalPanel(condition="input.menu == 'abatch'",
+                     column(12, uiOutput("dataMessage"),textOutput("dim"),tableOutput("abatch"))), 
     
-      conditionalPanel(condition="input.option == 'quality'",
-        radioButtons("plot_type", label="Rodzaj wykresu:",
-                                  choices = c("MVA","HEXBIN","BOXPLOT","SCATTER PLOT","HEATMAP","MA PLOT"), 
-                                  selected="MVA"),
-        
-     
+    conditionalPanel(condition="input.menu == 'quality'",
+                     column(6,
+                            selectInput("plot_type", label="Rodzaj wykresu:",
+                                        choices = c("MVA","HEXBIN","BOXPLOT","SCATTER PLOT","HEATMAP","MA PLOT"), 
+                                        selected="BOXPLOT")),
+                     column(6,
+                            sliderInput("range", "Zakres danych:", min = 1, max = 3 , value = c(1,3), step= 1)),
+                     uiOutput("max"),
     
-    sliderInput("range", "Zakres danych:",
-                min = 1, max = 4, value = c(1,4), step= 1))
+                    column(12, plotOutput("plots"))),
+    
+    conditionalPanel(condition="input.menu == 'clast'",
+                     column(6,
+                            selectInput("clust_type", label = "Rodzaj klasteryzacji:",
+                                        choices = c("K-means", "Hierarchiczna"), selected = "K-means"),
+                            sliderInput("num_clast", label = "Ilość klas:",
+                                         min = 2, max = 4, value = 1),
+                            actionButton("start", "Go!"), actionButton("stop", "Stop")),
+                            
+                     
+                     column(12, plotOutput("plot_clast")),
+                     column(12, tableOutput("table_clust")))
+    
+    
+  )
   
-  
-  
-),
-
-mainPanel(
-  tabsetPanel(type = "pills",
-              tabPanel("Opis"),
-              tabPanel("Dane",uiOutput("dataMessage"),textOutput("dim"),tableOutput("abatch")),
-              #tabPanel("Classification"),
-              #tabPanel("Clasterization"),                             
-              #tabPanel("Wykresy",imageOutput("image")),
-              tabPanel("Wyniki", uiOutput("resultsMessage"), uiOutput("results"),plotOutput("plots")),
-              tabPanel("Pobieranie wyników"))
 )
 )
-))
